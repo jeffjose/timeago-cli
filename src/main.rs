c31f0d7ceebd2b0fc4757f281e80c5f54fec1517
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-10-23 01:38:10 -0700

    setting up tests using speculate

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use chrono::{DateTime, FixedOffset};
 use chrono_humanize::{Accuracy, HumanTime, Tense};
 use structopt::StructOpt;
 
+#[cfg(test)]
+extern crate speculate;
+
+#[cfg(test)]
+use speculate::speculate;
+
 #[derive(StructOpt, Debug)]
 #[structopt(name = "A simple tool to humanize datetime")]
 struct Options {
@@ -68,8 +74,54 @@ fn main() -> Result<(), ParseError> {
     let dt = DateTime::parse_from_rfc2822(&options.datetime.to_string()).unwrap();
 
     let str = humanize(dt, options.long, options.precise);
-
     println!("{}", str.replace(" ", &options.separator.to_string()));
 
     Ok(())
+}
+
+speculate! {
+
+    use chrono::Duration;
+
+    fn create_dt(duration: Duration) -> DateTime<FixedOffset> {
+        let _dt = chrono::Local::now() - duration;
+
+        let dt = _dt.with_timezone(_dt.offset());
+
+        return dt;
+    }
+
+    describe "noflags"{
+
+        it "can do seconds" {
+            let dt = create_dt(chrono::Duration::seconds(30));
+            assert_eq!(humanize(dt, false, false), "30s");
+
+        }
+
+        it "can do minutes" {
+            let dt = create_dt(chrono::Duration::minutes(10));
+            assert_eq!(humanize(dt, false, false), "10m");
+        }
+
+        it "can do hours" {
+            let dt = create_dt(chrono::Duration::hours(2));
+            assert_eq!(humanize(dt, false, false), "2h");
+        }
+    }
+
+
+        //let dt = create_dt(chrono::Duration::seconds(30));
+        //assert_eq!(humanize(dt, true, false), "30 seconds");
+
+        //let dt = create_dt(chrono::Duration::minutes(10));
+        //assert_eq!(humanize(dt, true, false), "10 minutes");
+
+        //let dt = create_dt(chrono::Duration::hours(2));
+        //assert_eq!(humanize(dt, true, false), "2 hours");
+
+
+
+
+
 }
